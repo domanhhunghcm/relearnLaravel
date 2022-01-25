36fb12c34b9f81ae0c9804a32a947f7e0c712ea7
domanhhunghcm <domanhhunghcm@gmail.com>
2022-01-26 01:22:42 +0900

    controllerLearning

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
-    //
+    public function index($name=null)
+    {
+        return "from index homecontroller, have username: ".$name;
+    }
+
 }
